86e4a8c6320982fd4c48da22acf63a22fdc2106f
Prodegy03 <chri17052@byui.edu>
2022-10-27 21:02:44 -0600

    4

--- classes/player.py
+++ classes/player.py
@@ -4,4 +4,19 @@ from rock import rock
 
 class player():
     def __init__(self):
-        pass
+        """Constructs a new Actor."""
+        self._actors = {}
+        
+    def get_first_actor(self, group):
+        """Gets the first actor in the given group.
+        
+        Args:
+            group (string): The name of the group.
+            
+        Returns:
+            List: The first actor in the group.
+        """
+        result = None
+        if group in self._actors.keys():
+            result = self._actors[group][0]
+        return result
