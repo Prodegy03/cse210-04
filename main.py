0028d2c0d719fe936e02bc46b0252b9adfeed8e7
Prodegy03 <chri17052@byui.edu>
2022-10-28 14:47:14 -0600

    fine tune main

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
 
@@ -27,7 +26,7 @@ DEFAULT_ARTIFACTS = 40
 def main():
     
     # create the cast
-    cast = Cast()
+    player = player()
     
     # create the banner
     banner = player()
@@ -35,7 +34,7 @@ def main():
     banner.set_font_size(FONT_SIZE)
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
-    cast.add_actor("banners", banner)
+    player.add_actor("banners", banner)
     
     # create the robot
     x = int(MAX_X / 2)
@@ -47,7 +46,7 @@ def main():
     robot.set_font_size(FONT_SIZE)
     robot.set_color(WHITE)
     robot.set_position(position)
-    cast.add_actor("robots", robot)
+    player.add_actor("robots", robot)
     
     # create the artifacts
     with open(DATA_PATH) as file:
@@ -68,19 +67,19 @@ def main():
         b = random.randint(0, 255)
         color = Color(r, g, b)
         
-        artifact = Artifact()
-        artifact.set_text(text)
-        artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
-        artifact.set_position(position)
-        artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
+        item = falling_item()
+        falling_item.set_text(text)
+        falling_item.set_font_size(FONT_SIZE)
+        falling_item.set_color(color)
+        falling_item.set_position(position)
+        falling_item.set_message(message)
+        player.add_actor("Items", falling_item)
     
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
-    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    video_service = VideoService(MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
     director = Director(keyboard_service, video_service)
-    director.start_game(cast)
+    director.start_game(player)
 
 
 if __name__ == "__main__":
